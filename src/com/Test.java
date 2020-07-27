3148e65a8c29c5572490071426e72fb79fbf1a7d
刘永杰 <15028562592@163.com>
2020-07-27 21:03:05 +0800

    自动装箱与拆箱

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,9 +1,5 @@
 package com;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
 /**
  * @author liuyj
  * @Title: Test
@@ -12,15 +8,21 @@ import java.util.Objects;
  * @Description: TODO
  */
 public class Test {
+    private static long sum(){
+
+
+        //sum变量为Long与long时程序执行效率差别达数十倍之多
+        long sum=0L;
+        for (long i = 0L; i <=Integer.MAX_VALUE ; i++) {
+            sum+=i;
+        }
+
+        return sum;
+    }
     public static void main(String[] args) {
-        String  str="1243";
-        Objects.equals(str,"222");
-        Integer a=233;
-        a.equals(22);
-        Map<String,Object> map=new HashMap<>(1);
-        map.put("a",1);
-        map.put("3",3);
-       System.out.println(map.get("3"));
-       System.out.println(map.get("a"));
+        long start = System.currentTimeMillis();
+        System.out.println("----->"+sum());
+        long end = System.currentTimeMillis();
+        System.out.println(end - start + "ms");
     }
 }
